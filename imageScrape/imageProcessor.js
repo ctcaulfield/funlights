a0f119f62317538fb873441eebeeb9e958f3fd8b
Devon Bain <dwb264@cornell.edu>
2018-11-07 19:24:12 -0500

    start image resizing

---
+++ imageScrape/imageProcessor.js
@@ -0,0 +1,21 @@
+var express = require('express'); // web server application
+var app = express(); // webapp
+var http = require('http').Server(app); // connects http library to server
+var io = require('socket.io')(http); // connect websocket library to server
+var serverPort = 8000;
+
+// use express to create the simple webapp
+app.use(express.static('public')); // find pages in public directory
+
+// start the server and say what port it is on
+http.listen(serverPort, function() {
+  console.log('listening on *:%s', serverPort);
+});
+
+let imageArray = ["wave.png"];
+
+const sharp = require("sharp");
+
+sharp(imageArray[0])
+  .resize(32,32)
+  .toFile('output.png');
