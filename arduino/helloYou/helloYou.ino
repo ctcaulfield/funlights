6b81545f0ee6df7903204eb12d59514776969382
Devon Bain <dwb264@cornell.edu>
2018-12-02 01:09:11 -0500

    add emoji images and load them using progmem

--- arduino/helloYou/helloYou.ino
+++ arduino/helloYou/helloYou.ino
@@ -2,6 +2,11 @@
 
 #include <Adafruit_GFX.h>   // Core graphics library
 #include <RGBmatrixPanelHalfScan.h> // Hardware-specific library
+#include "anger.h"
+#include "joy.h"
+#include "sadness.h"
+#include "surprise.h"
+#include "q.h"
 
 #define CLK 11  // MUST be on PORTB! (Use pin 11 on Mega)
 #define OE  9
@@ -22,9 +27,19 @@ void setup() {
   //pinMode(led, OUTPUT);
   pinMode(button, INPUT);
   matrix.begin();
+
+  // demonstrate states
+  circleAnimation("anger");
+  circleAnimation("sadness");
+  circleAnimation("joy");
+  circleAnimation("surprise");
+  circleAnimation("q");
+
 }
 
 void loop() {
+  /*
+
   // read the character we recieve on the serial port from the RPi
   if(Serial.available()) {
     //inChar = (char)Serial.read();
@@ -57,5 +72,133 @@ void loop() {
     else{
       Serial.println("dark");
     }
-  }
+  } */
 }  
+
+
+void circleAnimation(String emotion) {
+
+  //White circles of thinking
+  for (int i = 23; i > 0; i--) {
+    matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+    delay(50);
+    matrix.fillScreen(matrix.Color333(0,0,0));
+  }
+
+  for (int i = 0; i < 23; i++) {
+    matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+    delay(50);
+    matrix.fillScreen(matrix.Color333(0,0,0));
+  }
+
+  for (int i = 23; i > 0; i--) {
+    matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+    delay(50);
+    matrix.fillScreen(matrix.Color333(0,0,0));
+  }
+  
+  if (emotion == "anger") {
+    
+    for (int i = 0; i < 28; i++) {
+      matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+      if (i-2 > 0) { matrix.drawCircle(15, 15, i-2, matrix.Color333(7, 7, 0)); }
+      if (i-4 > 0) { 
+        matrix.drawCircle(15, 15, i-4, matrix.Color333(7, 0, 0)); 
+      }
+      delay(50);
+      matrix.fillScreen(matrix.Color333(0,0,0));
+    }
+
+    delay(500);
+
+    int     i, len;
+    uint8_t *ptr = matrix.backBuffer(); // Get address of matrix data
+  
+    // Copy image from PROGMEM to matrix buffer:
+    memcpy_P(ptr, anger, sizeof(anger));
+
+  }
+
+  else if (emotion == "sadness") {
+     for (int i = 0; i < 28; i++) {
+      matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+      if (i-2 > 0) { matrix.drawCircle(15, 15, i-2, matrix.Color333(0, 7, 0)); }
+      if (i-4 > 0) { 
+        matrix.drawCircle(15, 15, i-4, matrix.Color333(0, 4, 7)); 
+      }
+      delay(50);
+      matrix.fillScreen(matrix.Color333(0,0,0));
+    }
+
+    delay(500);
+
+    int     i, len;
+    uint8_t *ptr = matrix.backBuffer(); // Get address of matrix data
+  
+    // Copy image from PROGMEM to matrix buffer:
+    memcpy_P(ptr, sadness, sizeof(sadness));
+  }
+
+  else if (emotion == "surprise") {
+     for (int i = 0; i < 28; i++) {
+      matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+      if (i-2 > 0) { matrix.drawCircle(15, 15, i-2, matrix.Color333(7, 7, 0)); }
+      if (i-4 > 0) { 
+        matrix.drawCircle(15, 15, i-4, matrix.Color333(7, 4, 0)); 
+      }
+      delay(50);
+      matrix.fillScreen(matrix.Color333(0,0,0));
+    }
+
+    delay(500);
+
+    int     i, len;
+    uint8_t *ptr = matrix.backBuffer(); // Get address of matrix data
+  
+    // Copy image from PROGMEM to matrix buffer:
+    memcpy_P(ptr, surprise, sizeof(surprise));
+  }
+
+  else if (emotion == "joy") {
+     for (int i = 0; i < 28; i++) {
+      matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+      if (i-2 > 0) { matrix.drawCircle(15, 15, i-2, matrix.Color333(7, 7, 0)); }
+      if (i-4 > 0) { 
+        matrix.drawCircle(15, 15, i-4, matrix.Color333(0, 7, 0)); 
+      }
+      delay(50);
+      matrix.fillScreen(matrix.Color333(0,0,0));
+    }
+
+    delay(500);
+
+    int     i, len;
+    uint8_t *ptr = matrix.backBuffer(); // Get address of matrix data
+  
+    // Copy image from PROGMEM to matrix buffer:
+    memcpy_P(ptr, joy, sizeof(joy));
+  }
+
+  else if (emotion == "q") {
+    for (int i = 0; i < 28; i++) {
+      matrix.drawCircle(15, 15, i, matrix.Color333(7, 7, 7));
+      if (i-2 > 0) { matrix.drawCircle(15, 15, i-2, matrix.Color333(0, 0, 7)); }
+      if (i-4 > 0) { 
+        matrix.drawCircle(15, 15, i-4, matrix.Color333(4, 0, 7)); 
+      }
+      delay(50);
+      matrix.fillScreen(matrix.Color333(0,0,0));
+    }
+
+    delay(500);
+
+    int     i, len;
+    uint8_t *ptr = matrix.backBuffer(); // Get address of matrix data
+  
+    // Copy image from PROGMEM to matrix buffer:
+    memcpy_P(ptr, q, sizeof(q));
+    
+  }
+
+  delay(2000);
+} 
---
+++ python/image.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+image.py
+
+Converts an image into matrix.drawPixel commands for a 32x32 LED display
+"""
+
+from skimage import io, transform
+
+image = io.imread("q.png")
+
+image = transform.resize(image, [32,32])
+#io.imsave("anger-sm.png", image) #uncomment to save 32x32 img
+
+print image[0][0]
+f = open("matrix.txt", "w")
+
+for i in range(32):
+    for j in range(32):
+        px = image[i][j]
+        if (px[3] == 0):
+            px = ["0", "0", "0"] #transparent = black
+        else:
+            px = [str(int(px[k]*7)) for k in range(3)] #scale colors to 0-7
+        colorString = "matrix.Color333(" + ",".join(px) + ")"
+        f.write("matrix.drawPixel(" + str(i) + "," + str(j) + "," + colorString + ");\n")
+
+f.close()
+        
